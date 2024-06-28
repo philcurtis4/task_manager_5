9853e88cfb45ffd0b0caa2471dd8ab59bc469545
philcurtis422 <philcurtis422@gmail.com>
2024-06-28 17:18:05 -0400

    finished modal started on logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,36 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
+const $titleInput = $('#title-input');
+const $descriptionInput = $('description-input');
+const $dateInput = $('date-input');
+const dateStr = $dateInput.val();
+const dateTime = dayjs(dateStr);
+const dateFormat = dateTime.format('MMM DD YYYY');
 
+console.log(dateFormat);
+
+const taskCounter = 1;
+// Todo: create a function to generate a unique task id
+function generateTaskId(eventObj) {
+    
+    const taskId = taskCounter;
+    taskCounter++;
+    return taskId;
+    
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const card = $('<div>').addClass("card");
 
+    const cardTitle = $('<h2>').text('cool');
+
+    const cardDescription = $('<p>>').text('nice');
+
+    card.append(cardTitle, cardDescription);
+
+    $('#todo-cards').append(card);
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -34,5 +56,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    $('#date-input').datepicker();
 });
+
+console.log(createTaskCard());
