06246bfcd779c78a6895239764eca4dfcc8cd373
philcurtis422 <philcurtis422@gmail.com>
2024-06-30 21:16:07 -0400

    got to stop double loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,8 +38,7 @@ function createTaskCard(task) {
     
     tasks.forEach(function (eventObj) {
         const day = dayjs();
-        console.log(day);
-        console.log(eventObj.date);
+        
         const taskEl = $(
             `<article data-id="${eventObj.id}" class="mt-3 card drag ui-widget-content" >
                 <h2>${eventObj.title}</h2>
@@ -169,15 +168,17 @@ function handleDeleteTask(event){
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
     const box = $(event.target);
+    const maybe = $(event.target.children);
+    console.log(box);
     const article = $(ui.draggable[0])
     const taskId = article.data('id');
-    console.log(box);
+    
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     const task = tasks.find(function (taskObj) {
         if(taskObj.id === taskId) return true;
     })
-    console.log(task);
+    
 
     if(box.hasClass('to-do')) {
         task.completed = '1';
@@ -189,7 +190,7 @@ function handleDrop(event, ui) {
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
 
-    box.append(article);
+    maybe.append(article);
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
